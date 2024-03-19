1aa767f04e663e99e4644f9f635e0d7156c69b40
vincent <fongvincent039@gmail.com>
2024-03-19 15:40:29 +0800

    2024.3.19提交

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -64,7 +64,7 @@ function FeaturedCollection({ collection }) {
 function RecommendedProducts({ products }) {
   return (
     <div className="recommended-products">
-      <h2>Recommended Products test 12312331232</h2>
+      <h2>Recommended Products test 123123ssss31232</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {({ products }) => (
